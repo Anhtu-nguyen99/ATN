5600c829686bbdcfd67d110e14af09a14ff77468
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-19 09:02:02 +0700

    Update product_create.php

--- product_create.php
+++ product_create.php
@@ -1,4 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
 	<?php
 	$id = $_REQUEST["id"];
 	$name = $_REQUEST["name"];
@@ -14,4 +21,5 @@
 	echo $desc;
 	echo $price;
 	?>
+</body>
 </html>
