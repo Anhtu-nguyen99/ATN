a63188bcdc901e9e5d5c8eeee0c42fb847c6bfa7
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-23 04:34:36 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -3,7 +3,7 @@ function customer() {
 	label[0].outerHTML = '<label for="productid">Customer ID<sup>*</sup></label>'; 
 	label[1].outerHTML = '<label for="productname">Customer Name<sup>*</sup></label>';
 	label[2].outerHTML = '<label for="productcat">Phone Number</label>';
-	label[3].outerHTML = '<label for="email">Eamil</label>';
+	label[3].outerHTML = '<label for="email">Email</label>';
 	label[4].outerHTML = '<label for="pice">Address</label>';
 
 	var input = document.getElementsByTagName("input");
@@ -14,10 +14,10 @@ function customer() {
 	textarea.outerHTML = '<input type="text" name="email" id="email">';
 
 	var form = document.getElementsByTagName("form");
-	form.setAttribute("action", "customer_create.php");
+	form[0].setAttribute("action", "customer_create.php");
 
 
 	
 }
 
-alert("anh tuan");
+alert("anh tu");
