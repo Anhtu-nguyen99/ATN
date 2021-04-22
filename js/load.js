f642fd92810aeb226b256df438a7d7673f99098e
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-23 03:49:20 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -2,6 +2,9 @@ function customer() {
 	var title = document.getElementsByTagName("title");
 	title.innerHTML = "CUSTOMER-ATM";
 
+	var label = document.getElementsByTagName("label");
+	label[0].innerHTML = "customer ID"; 
+
 	
 }
 
