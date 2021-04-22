7043309099af39b135b3924851173eb975328c5a
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-23 03:50:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en" >
 <head>
     <meta charset="UTF-8">
+    <script src="js/load.js"></script>
     <title>PRODUCT-ATN</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="jqueryMobile/jquery.mobile-1.4.5.min.css">
-    <script src="js/load.js"></script>
+
     <script src="jqueryMobile/jquery-1.11.1.min.js"></script>
     <script src="jqueryMobile/jquery.mobile-1.4.5.min.js"></script>
 </head>
