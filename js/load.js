dc800c147a17876286fa0e29dbe59eecef108a84
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-26 21:09:15 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -55,7 +55,7 @@ function product() {
 	add.firstChild.remove();
 
 	var cancle = document.getElementsByClassName("content-list");
-	cancle[0].childNodes.remove();
+	cancle[0].remove();
 
 	var form = document.getElementsByTagName("form");
 	form[0].setAttribute("action", "product_create.php");
