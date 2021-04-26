8714a0c33e5fa73ea3f610354b6015eb9f2fcea8
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-04-26 09:38:20 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -55,7 +55,7 @@ function addProductItem() {
 	alert("run");
     var n = i;
     var newli = '<li id=\"'+ n +'\">Product ID: <input name="invoice_productid[]" type="text"> Quantity: <input name="invoice_product_quantity[]" type="text">'+
-    '<a id="cancle'+n+'" class="ui-btn ui-btn-b ui-corner-all ui-btn-icon-left ui-icon-plus" onclick=deleteProductItem() href="#">Delete</a></li>';
+    '<a id="cancle'+n+'" class="ui-btn ui-btn-b ui-corner-all ui-btn-icon-left ui-icon-plus" onclick=\"deleteProductItem()\" href="#">Delete</a></li>';
     $("ul.content-list").append(newli);
     i++;
     function deleteProductItem() {
