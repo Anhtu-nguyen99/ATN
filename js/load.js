d2a9068e37bce8842314eba9fd869acffd0892bd
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-05-02 01:49:07 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -95,13 +95,13 @@ function bill() {
 var i = 0;
 function addProductItem() {
     var n = i;
-    var newli = '<li id=\"'+ n +
-    '\">Product ID: <input class="getName" name="invoice_productId[]" type="text" onblur="getName()"> <span>hello</span> Quantity: <input name="invoice_product_quantity[]" type="text">'+
-    '<a id=\"cancle'+n+'\" href="#">Delete</a></li>';
+    var newli = '<li id=\"' + n +
+    '\">Product ID: <input class="getName" name="invoice_productId[]" type="text" onblur="getName()"> <span>hello</span> Quantity: <input name="invoice_product_quantity[]" type="text">' +
+    '<a id=\"cancle' + n + '\" href="#">Delete</a></li>';
     $("ul.content-list").append(newli);
     i++;
 
-	$("#cancle"+n).click(function(){
+	$("#cancle" + n).click(function(){
 		var item = document.getElementById(n);
 		item.remove();
 	});	
@@ -110,17 +110,25 @@ function addProductItem() {
 
 function getName() {
 	var proId = $(".getName").val();
+	var xhttp = new XMLHttpRequest();
+	xhttp.ondreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			document.getElementsByTagName("span").innerHTML = xhttp.responseText;
+		}
+	};
+	xhttp.open("GET", "invoice_create.php?id=", true);
+	xhttp.send();
 
-    $.ajax({
-    url: 'index.php?id=&name=',
-    type: 'GET',
-    data: { id: proId, name: name },
-	})
-	.done(function(data) {
-		if (proId )
-		$("span").html(data);
-		$("span").show();
-	});
+ //    $.ajax({
+ //    url: 'index.php?id=&name=',
+ //    type: 'GET',
+ //    data: { id: proId, name: name },
+	// })
+	// .done(function(data) {
+	// 	if (proId )
+	// 	$("span").html(data);
+	// 	$("span").show();
+	// });
 }
 
 
