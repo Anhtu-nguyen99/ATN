86a3cb1ffc0ad001d6cecede6e9acdbd7746a9f0
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-05-05 03:55:06 +0700

    Update home_page.php

--- home_page.php
+++ home_page.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>ATN Company</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="jqueryMobile/jquery.mobile-1.4.5.min.css">
-	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
-
-    <script src="jqueryMobile/jquery-1.11.1.min.js"></script>
-    <script src="jqueryMobile/jquery.mobile-1.4.5.min.js"></script>
-</head>
-<body>
-	<header>
-		<div data-role="navbar">
-			<ul>
-				<li><a href="index.php" data-role="button">Admin page</a></li>
-				<li><a href="home_page.php" data-role="button">Home page</a></li>
-			</ul>
-		</div>
-		<div>
-			<img src="img/head.jpg" alt="head car">
-		</div>
-	</header>
-	<main>
-		<h3>ALL Car</h3>
-		<div class="row">
-			<div class="col-xs-12">
-				<?php foreach ($products as $product) { ?>
-				<div class="col-xs-6 col-sm-3">
-					<img class="image" src="img/530.jpg" alt="530 m sport">
-					<h5><?=$product->getName();?></h5>
-					<p><?=$product->getPrice();?></p>
-				</div>
-				<?php } ?>
-			</div>
-		</div>
-	</main>
-	<footer>
-		<div data-role="footer" data-position="fixed">
-        <h4>Navigation</h4>
-        <div data-role="navbar">
-            <ul>
-                <li><a href="home_page.php" data-role="button">Home</a></li>
-                <li><a href="#invoice" onclick="bill()" data-role="button">Invoice</a></li>
-            </ul>
-        </div>
-    </div>
-	</footer>
-	<?php
+<?php
 	$host = "ec2-54-167-152-185.compute-1.amazonaws.com";
 	$database = "d7rjokn0julj07";
 	$user = "gqkbiudkurbksn";
@@ -101,5 +51,56 @@ class Product {
 	}
 }
 	?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ATN Company</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="jqueryMobile/jquery.mobile-1.4.5.min.css">
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+    <script src="jqueryMobile/jquery-1.11.1.min.js"></script>
+    <script src="jqueryMobile/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+	<header>
+		<div data-role="navbar">
+			<ul>
+				<li><a href="index.php" data-role="button">Admin page</a></li>
+				<li><a href="home_page.php" data-role="button">Home page</a></li>
+			</ul>
+		</div>
+		<div>
+			<img src="img/head.jpg" alt="head car">
+		</div>
+	</header>
+	<main>
+		<h3>ALL Car</h3>
+		<div class="row">
+			<div class="col-xs-12">
+				<?php foreach ($products as $product) { ?>
+				<div class="col-xs-6 col-sm-3">
+					<img class="image" src="img/530.jpg" alt="530 m sport">
+					<h5><?=$product->getName();?></h5>
+					<p><?=$product->getPrice();?></p>
+				</div>
+				<?php } ?>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div data-role="footer" data-position="fixed">
+        <h4>Navigation</h4>
+        <div data-role="navbar">
+            <ul>
+                <li><a href="home_page.php" data-role="button">Home</a></li>
+                <li><a href="#invoice" onclick="bill()" data-role="button">Invoice</a></li>
+            </ul>
+        </div>
+    </div>
+	</footer>
+	
 </body>
 </html>
