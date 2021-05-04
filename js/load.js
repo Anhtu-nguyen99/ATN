fc85d4bf53eac1ea8835aa1774c2a7bd68f26670
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-05-05 00:27:18 +0700

    Update load.js

--- js/load.js
+++ js/load.js
@@ -96,7 +96,7 @@ var i = 0;
 function addProductItem() {
     var n = i;
     var newli = '<li id=\"' + n +
-    '\">Product ID: <input class="proId" name="invoice_productId[]" type="text" onblur="getName()"> <span id="ajaxSpan">hello</span> Quantity: <input name="invoice_product_quantity[]" type="text">'+
+    '\">Product ID: <input name="invoice_productId[]" type="text" onblur="getName()"> <span id="ajaxSpan">hello</span> Quantity: <input name="invoice_product_quantity[]" type="text">'+
     '<a id=\"cancle' + n + '\" href="#">Delete</a></li>';
     $("ul.content-list").append(newli);
     i++;
