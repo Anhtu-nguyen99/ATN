42885221c6dc1194a42aaf727be2a1e05f4f8d98
Anhtu-nguyen99 <natgeneral99@gmail.com>
2021-05-05 10:32:48 +0700

    Update product_get_name.php

--- product_get_name.php
+++ product_get_name.php
@@ -7,23 +7,23 @@
 </head>
 <body>
 	<?php
-	// $host = "ec2-54-167-152-185.compute-1.amazonaws.com";
-	// $database = "d7rjokn0julj07";
-	// $user = "gqkbiudkurbksn";
-	// $password = "b0f9ad0d6bc76622ca310653f9129f2cab440d8e23e3061ef22d3b1934906c01";
-	// $port = "5432";
+	$host = "ec2-54-167-152-185.compute-1.amazonaws.com";
+	$database = "d7rjokn0julj07";
+	$user = "gqkbiudkurbksn";
+	$password = "b0f9ad0d6bc76622ca310653f9129f2cab440d8e23e3061ef22d3b1934906c01";
+	$port = "5432";
 
-	// $connection = pg_connect("host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require");
+	$connection = pg_connect("host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require");
 
 	$ajaxId = $_GET["id"];
 
 	echo "<p>".$ajaxId."</p>";
 
-    // $pg = 'SELECT name FROM public."Product" WHERE id = \''.$ajaxId.'\'';
-    // $result = pg_query($connection,$pg);
-    // while ($row = pg_fetch_row($result)) {
-    // 	echo "<p>".$row[0]."</p>";
-    // }
+    $pg = 'SELECT name FROM public."Product" WHERE id = \''.$ajaxId.'\'';
+    $result = pg_query($connection,$pg);
+    while ($row = pg_fetch_row($result)) {
+    	echo "<p>".$row[0]."</p>";
+    }
 ?>
 </body>
 </html>
